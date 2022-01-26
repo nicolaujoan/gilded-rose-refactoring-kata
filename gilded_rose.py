ff4292a0cbdff8a9cfe764dcfbfcefe125abeda5
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-01-26 11:26:09 +0100

    remove redundant comments

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,42 +10,11 @@ class GildedRose(object):
         for item in self.stock:
             item.updateQuality()
 
-
-    # def update_quality(self):
-    #     for item in self.items:
-    #         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-    #             if item.quality > 0:
-    #                 if item.name != "Sulfuras, Hand of Ragnaros":
-    #                     item.quality = item.quality - 1
-    #         else:
-    #             if item.quality < 50:
-    #                 item.quality = item.quality + 1
-    #                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
-    #                     if item.sell_in < 11:
-    #                         if item.quality < 50:
-    #                             item.quality = item.quality + 1
-    #                     if item.sell_in < 6:
-    #                         if item.quality < 50:
-    #                             item.quality = item.quality + 1
-    #         if item.name != "Sulfuras, Hand of Ragnaros":
-    #             item.sell_in = item.sell_in - 1
-    #         if item.sell_in < 0:
-    #             if item.name != "Aged Brie":
-    #                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
-    #                     if item.quality > 0:
-    #                         if item.name != "Sulfuras, Hand of Ragnaros":
-    #                             item.quality = item.quality - 1
-    #                 else:
-    #                     item.quality = item.quality - item.quality
-    #             else:
-    #                 if item.quality < 50:
-    #                     item.quality = item.quality + 1
-
-    def add_item():
+    def add_item(self):
         pass
 
 
-# this class cannot be modified
+# this class cannot be modified ( kata rules !!! )
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -57,7 +26,7 @@ class Item:
 
 
 # our informal interface
-class Updateable():
+class Updateable:
     def updateQuality(self) -> None:
         """Update the quality of an item"""
         pass
